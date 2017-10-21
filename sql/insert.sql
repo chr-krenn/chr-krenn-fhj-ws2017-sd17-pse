b531f153152162ea9b794d6472cea80cb9cc1eb4
student <student@localhost.localdomain>
2017-10-21 19:59:50 +0200

    insert sample data

---
+++ sql/insert.sql
@@ -0,0 +1,3 @@
+insert into usertype(id, type)  values (1, "admin");
+insert into profile(id, description)  values (1, "empty profile");
+insert into users(id, uname, passwd,profile_id,usertype_id)  values (1,'frank','pass',1,1);
