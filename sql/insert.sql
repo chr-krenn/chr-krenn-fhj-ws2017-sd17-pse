718ddb8ec6ef55a577b74955cc768621c97461dc
student <student@localhost.localdomain>
2017-10-25 13:02:09 +0200

    insert sample data

--- sql/insert.sql
+++ sql/insert.sql
@@ -13,6 +13,16 @@ INSERT INTO contact(id, user_id, contact_id) VALUES (4,2,3);
 -- write a pm from admin to alice
 INSERT INTO private_message(id, from_id, to_id, text) VALUES (1,1,3, 'this is a private message');
 
--- create a community
---INSERT INTO community(id,community_id
--- TODO: create junction table, this does not work
+-- create community SWD15
+INSERT INTO community(id,name) VALUES (1, 'SWD15');
+
+-- admin posts on community SWD15
+INSERT INTO post(id, user_id, community_id,text) VALUES (1,1,1,'first post from admin!');
+
+-- admin talks to himself..
+INSERT INTO post(id, user_id, community_id,parent_post_id, text) VALUES (2,1,1,1,'This is a reply from admin');
+
+-- bob posts on community SWD15, alice posts a reply
+INSERT INTO post(id, user_id, community_id,text) VALUES (3,2,1,'This is a post from bob!');
+INSERT INTO post(id, user_id, community_id,text) VALUES (4,3,1,'This is a reply from alice to bob.');
+
