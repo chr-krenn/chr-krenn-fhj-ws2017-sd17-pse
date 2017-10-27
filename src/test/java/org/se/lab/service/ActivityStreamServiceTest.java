ddb76e3b35b66f8c5066dce2fab913ba63ff4de5
Pointinger Matthias <matthias.pointinger@magna.com>
2017-10-27 09:29:18 +0200

    Added empty service tests

---
+++ src/test/java/org/se/lab/service/ActivityStreamServiceTest.java
@@ -0,0 +1,25 @@
+package org.se.lab.service;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ActivityStreamServiceTest{
+
+	@Before
+	public void setUp() throws Exception{
+	}
+
+	@After
+	public void tearDown() throws Exception{
+	}
+
+	@Test
+	public void test(){
+		// TODO when service is implemented
+		fail("Not yet implemented");
+	}
+
+}
