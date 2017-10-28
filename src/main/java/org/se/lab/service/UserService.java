f0de30b98c7f44cb7860dfafc022e5617d20b560
Bogdan <bogdan.reimer@edu.fh-joanneum.at>
2017-10-28 09:35:12 +0200

    renamed article to user

--- src/main/java/org/se/lab/service/UserService.java
+++ src/main/java/org/se/lab/service/UserService.java
@@ -1,101 +1,100 @@
 package org.se.lab.service;
 
-import java.util.List;
-
-import javax.ejb.Stateless;
-import javax.inject.Inject;
-
 import org.apache.log4j.Logger;
 import org.se.lab.data.User;
 import org.se.lab.data.UserDAO;
 
-@Stateless
-public class UserService{
-	private final Logger LOG=Logger.getLogger(UserService.class);
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import java.util.List;
 
-	@Inject
-	private UserDAO dao;
+@Stateless
+public class UserService {
+    private final Logger LOG = Logger.getLogger(UserService.class);
+
+    @Inject
+    private UserDAO dao;
 
 	/*
-	 * API Operations
+     * API Operations
 	 */
 
-	public List<User> findByName(String name){
-		return null;
-	}
+    public List<User> findByName(String name) {
+        return null;
+    }
 
-	public void insert(User article){
-		LOG.debug("insert "+article);
+    public void insert(User user) {
+        LOG.debug("insert " + user);
 
-		try{
-			dao.insert(article);
-		}catch(Exception e){
-			LOG.error("Can't insert article "+article,e);
-			throw new ServiceException("Can't insert article "+article);
-		}
-	}
+        try {
+            dao.insert(user);
+        } catch (Exception e) {
+            LOG.error("Can't insert user " + user, e);
+            throw new ServiceException("Can't insert user " + user);
+        }
+    }
 
-	public void delete(User article){
-		LOG.debug("delete "+article);
+    public void delete(User user) {
+        LOG.debug("delete " + user);
 
-		// TODO
-	}
+        // TODO
+    }
 
-	public User login(String username,String password){
-		LOG.debug("login for "+username);
-		// TODO +hashing
-		return null;
-	}
+    public User login(String username, String password) {
+        LOG.debug("login for " + username);
+        // TODO +hashing
+        return null;
+    }
 
-	public void addContact(User article,User contact){
-		LOG.debug("add contact to "+article);
+    public void addContact(User user, User contact) {
+        LOG.debug("add contact to " + user);
 
-		// TODO
-	}
+        // TODO
+    }
 
-	public void removeContact(User article,User contact){
-		LOG.debug("remove contact from "+article);
+    public void removeContact(User user, User contact) {
+        LOG.debug("remove contact from " + user);
 
-		// TODO
-	}
+        // TODO
+    }
 
-	public void update(User article){
-		LOG.debug("update "+article);
+    public void update(User user) {
+        LOG.debug("update " + user);
 
-		// TODO
-	}
+        // TODO
+    }
 
-	public List<User> findAll(){
-		LOG.debug("find all articles");
+    public List<User> findAll() {
+        LOG.debug("find all users");
 
-		try{
-			List<User> list=dao.findAll();
-			return list;
-		}catch(Exception e){
-			LOG.error("Can't find all articles!",e);
-			throw new ServiceException("Can't find all articles!");
-		}
-	}
+        try {
+            List<User> list = dao.findAll();
+            return list;
+        } catch (Exception e) {
+            LOG.error("Can't find all users!", e);
+            throw new ServiceException("Can't find all users!");
+        }
+    }
 
 	/*
 	 * TODO check if methods delete(id), findById(id) required
 	 */
 
-	public void delete(int id){
-		LOG.info("delete: "+id);
+    public void delete(int id) {
+        LOG.info("delete: " + id);
 
-		// TODO
-	}
+        // TODO
+    }
 
-	public User findById(int id){
-		LOG.debug("find User with id="+id);
+    public User findById(int id) {
+        LOG.debug("find User with id=" + id);
 
-		try{
-			User article=dao.findById(id);
-			return article;
-		}catch(Exception e){
-			LOG.error("Can't find article with id "+id,e);
-			throw new ServiceException("Can't find article with id "+id);
-		}
-	}
+        try {
+            User user = dao.findById(id);
+            return user;
+        } catch (Exception e) {
+            LOG.error("Can't find user with id " + id, e);
+            throw new ServiceException("Can't find user with id " + id);
+        }
+    }
 }
