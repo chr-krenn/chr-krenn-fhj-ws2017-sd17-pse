0591508063a768f239c7c146fb47a75ef7bb2494
Christian Hofer <christian.hofer@edu.fh-joanneum.at>
2017-10-29 20:26:04 +0100

    Add files via upload
    Add Test Suite by Stefan

---
+++ src/test/java/org/se/lab/data/DataTestSuite.java
@@ -0,0 +1,15 @@
+package org.se.lab.data;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	CommunityTest.class,
+	PostTest.class,
+	UserProfileTest.class
+})
+public class DataTestSuite {
+	
+}
