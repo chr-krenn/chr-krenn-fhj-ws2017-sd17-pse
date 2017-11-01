3687444ae36154661dd71d49dfaa829acc6be414
Bernhard Haring <bernhard.haring@edu.fh-joanneum.at>
2017-11-01 09:42:49 +0100

    - activity stream service

--- src/main/java/org/se/lab/service/ActivityStreamService.java
+++ src/main/java/org/se/lab/service/ActivityStreamService.java
@@ -12,41 +12,58 @@ import org.se.lab.data.PostDAO;
 import org.se.lab.data.User;
 
 @Stateless
-public class ActivityStreamService{
-	private final Logger LOG=Logger.getLogger(ActivityStreamService.class);
+public class ActivityStreamService {
+    private final Logger LOG = Logger.getLogger(ActivityStreamService.class);
 
-	@Inject
-	private PostDAO dao;
+    @Inject
+    private PostDAO dao;
 
-	public void insert(Post article){
-		insert(article,null);
-	}
+    public void insert(Post article) {
+        insert(article, null);
+    }
 
-	public void insert(Post article,Community community){
-		LOG.debug("insert "+article);
+    public void insert(Post post, Community community) {
+        LOG.debug("insert " + post);
 
-		// TODO if community is null, => global post
-	}
+        try {
+            if (community == null) {
+                dao.insert(post);
+            } else {
+                dao.insert(post, community);
+            }
+        } catch (Exception e) {
+            LOG.error("Can't insert post " + post);
+            throw new ServiceException("Can't insert post " + post, e);
+        }
+    }
 
-	public void delete(Post article){
-		LOG.debug("delete "+article);
+    public void delete(Post post) {
+        LOG.debug("delete " + post);
+        try {
+            dao.delete(post);
+        } catch (Exception e) {
+            LOG.error("Can't delete post " + post);
+            throw new ServiceException("Can't delete post " + post, e);
+        }
+    }
 
-		// TODO
-	}
+    public void update(Post post) {
+        LOG.debug("update " + post);
+        try {
+            dao.delete(post);
+        } catch (Exception e) {
+            LOG.error("Can't update post " + post);
+            throw new ServiceException("Can't update post " + post, e);
+        }
+    }
 
-	public void update(Post article){
-		LOG.debug("update "+article);
+    public List<Post> getPostsForUser(User user) {
+        LOG.debug("getting posts relevant for " + user);
+        return dao.getPostsForUser(user);
+    }
 
-		// TODO
-	}
-
-	public List<Post> getPostsForUser(User user){
-		LOG.debug("getting posts relevant for "+user);
-		return null;
-	}
-
-	public List<Post> getPostsForCommunity(User user,Community community){
-		LOG.debug("getting posts relevant for "+user+" and "+community);
-		return null;
-	}
+    public List<Post> getPostsForCommunity(Community community) {
+        LOG.debug("getting posts relevant for " + community);
+        return dao.getPostsForCommunity(community);
+    }
 }
