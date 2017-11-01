029a0785872c263a3dc2a3bec971f695b5b4c739
Bernhard Haring <bernhard.haring@edu.fh-joanneum.at>
2017-11-01 09:05:54 +0100

    added some service tests

--- src/test/java/org/se/lab/service/UserServiceTest.java
+++ src/test/java/org/se/lab/service/UserServiceTest.java
@@ -1,25 +1,86 @@
 package org.se.lab.service;
 
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
+import static org.easymock.EasyMock.replay;
 import static org.junit.Assert.fail;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.easymock.EasyMockRule;
+import org.easymock.EasyMockRunner;
+import org.easymock.Mock;
+import org.easymock.TestSubject;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.se.lab.data.User;
+import org.se.lab.data.UserDAO;
 
-public class UserServiceTest{
+import java.sql.SQLDataException;
 
-	@Before
-	public void setUp() throws Exception{
-	}
+@RunWith(EasyMockRunner.class)
+public class UserServiceTest {
 
-	@After
-	public void tearDown() throws Exception{
-	}
+    public static final int ID = 1;
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
 
-	@Test
-	public void test(){
-		// TODO when service is implemented
-		fail("Not yet implemented");
-	}
+    @TestSubject
+    private UserService userService = new UserService();
 
+    @Rule
+    public EasyMockRule mocks = new EasyMockRule(this);
+
+    @Mock
+    private UserDAO userDAO;
+
+    private User user;
+
+    @Before
+    public void setUp() throws Exception {
+        user = new User(ID, USERNAME, PASSWORD);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void insert_Successful() {
+        expect(userDAO.insert(user)).andReturn(user);
+
+        userService.insert(user);
+    }
+
+    @Ignore //cant handle because of mock doesn`t have an exception in method signature
+    @Test(expected = ServiceException.class)
+    public void insert_ThrowException() {
+        expect(userDAO.insert(user)).andThrow(new Exception());
+        replay();
+        userService.insert(user);
+    }
+
+    @Test
+    public void delete_Successful() {
+        User user = new User(ID, USERNAME, PASSWORD);
+
+        userDAO.delete(user);
+        expectLastCall();
+
+        userService.delete(user);
+    }
+
+    @Test
+    public void login_Successful() {
+        expect(userDAO.loadByUsername(USERNAME)).andReturn(user);
+        replay(userDAO);
+
+        userService.login(user.getUsername(), user.getPassword());
+    }
+
+    @Test(expected = ServiceException.class)
+    public void login_Fail() {
+        expect(userDAO.loadByUsername(USERNAME)).andReturn(user);
+        replay(userDAO);
+
+        userService.login(user.getUsername(), "wrongPassword");
+    }
 }
