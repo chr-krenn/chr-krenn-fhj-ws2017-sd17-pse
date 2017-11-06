3c9fb2aa6601a132e29ea53f5e0bb5fdbc55db5d
sattlerb10 <bjoern.sattler@edu.fh-joanneum.at>
2017-11-06 18:35:48 +0100

    Added Session check. Redirect to Login controlling

--- src/main/java/org/se/lab/web/UserOverviewBean.java
+++ src/main/java/org/se/lab/web/UserOverviewBean.java
@@ -2,63 +2,108 @@
 package org.se.lab.web;
 
 import org.apache.log4j.Logger;
+import org.se.lab.data.Community;
 import org.se.lab.data.User;
 import org.se.lab.data.UserProfile;
 import org.se.lab.service.UserService;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Named
 @RequestScoped
 public class UserOverviewBean {
 
-	private final Logger LOG = Logger.getLogger(UserOverviewBean.class);
+    private final Logger LOG = Logger.getLogger(UserOverviewBean.class);
 
-	@Inject
-	UserService service;
+    @Inject
+    UserService service;
+    /*
+     * Properties for Session
+     */
+    Flash flash;
+    FacesContext context;
+    private List<UserProfile> profiles;
+    private UserProfile selectedProfile;
+    private String id = "";
+    private int userId = 0;
 
-	private List<UserProfile> profiles;
-	private UserProfile selectedProfile;
 
-	@PostConstruct
-	public void init() {
+    @PostConstruct
+    public void init() {
 
-		// DummyData
-		profiles = new ArrayList<UserProfile>();
-		User user1 = new User(1, "User1", "***");
-		User user2 = new User(2, "User2", "***");
-		UserProfile p1 = new UserProfile( "Max", "Muster", "max@muster.at", "0316-555", "0664/1234567", "ExistingUser");
-		UserProfile p2 = new UserProfile( "Heinz", "Fischer", "heinz@fischer.at", "0316-555", "0664/1234567", "NewUser");		
-		user1.setUserProfile(profiles.get(0));
-		user2.setUserProfile(profiles.get(1));
-		profiles.add(p1);
-		profiles.add(p2);
+        //TODO Check if Session exists
+        context = FacesContext.getCurrentInstance();
 
-		// Activate if DAO works
-		// profiles = service.getAllUserProfiles();
-		
-		
-	}
+		/*
+         * FG Info Flash: We need flash to make the param survive one redirect request
+		 * otherwise param will be null
+		 */
+        flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
 
-	public List<UserProfile> getProfiles() {
-		return profiles;
-	}
+		/*
+         * Holen der UserId vom User welcher aktuell eingeloggt ist(Session)
+		 *
+		 */
 
-	public void setProfiles(List<UserProfile> profiles) {
-		this.profiles = profiles;
-	}
+        Map<String, Object> session = context.getExternalContext().getSessionMap();
 
-	public UserProfile getSelectedProfile() {
-		return selectedProfile;
-	}
 
-	public void setSelectedProfile(UserProfile selectedProfile) {
-		this.selectedProfile = selectedProfile;
-	}
+        id = context.getExternalContext().getRequestParameterMap().get("userid");
+
+        flash.put("uid", id);
+
+
+        String userProfId = (String) context.getExternalContext().getFlash().get("uid");
+
+
+        System.out.println("userProfId: " + userProfId);
+
+        if (session.size() != 0 && session.get("user") != null) {
+
+            userId = (int) session.get("user");
+            System.out.println("SESSIOn UID: " + userId);
+        } else {
+			/*
+			 * If session is null - redirect to login page!
+			 *
+			 */
+            try {
+                context.getExternalContext().redirect("/pse/login.xhtml");
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        // Activate if DAO works
+        profiles = service.getAllUserProfiles();
+
+
+    }
+
+    public List<UserProfile> getProfiles() {
+        return profiles;
+    }
+
+    public void setProfiles(List<UserProfile> profiles) {
+        this.profiles = profiles;
+    }
+
+    public UserProfile getSelectedProfile() {
+        return selectedProfile;
+    }
+
+    public void setSelectedProfile(UserProfile selectedProfile) {
+        this.selectedProfile = selectedProfile;
+    }
 
 }
