e740e79829a2d2d8d5c4d71ddff5c6c41ab55e23
hofchris15 <christian.hofer@edu.fh-joanneum.at>
2017-11-08 16:34:30 +0100

    prepare sql init

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,4 @@
 
 CREATE DATABASE pse;
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON `pse`.* TO 'student'@'localhost';
-CREATE DATABASE pse_test;
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON `pse_test`.* TO 'student'@'localhost';
 use pse;
