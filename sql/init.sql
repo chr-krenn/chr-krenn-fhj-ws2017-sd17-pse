6eb2f14f7c34ed86dadad5fd9bd4be7f631d9076
moderste15 <stefan.moder@edu.fh-joanneum.at>
2017-11-09 19:37:46 +0100

    DB dump > create.sql 2

--- sql/init.sql
+++ sql/init.sql
@@ -2,5 +2,5 @@
 CREATE DATABASE pse;
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON `pse_test`.* TO 'student'@'localhost';
 use pse;
-source sql/truncate.sql
+source sql/create.sql
 source sql/insert.sql
