dbda34df23b34e823e5743add64ab91556ebd2be
moderste15 <stefan.moder@edu.fh-joanneum.at>
2017-11-09 11:43:18 +0100

    Added truncate.sql to init.sql to stop duplicate inserts

--- sql/init.sql
+++ sql/init.sql
@@ -2,4 +2,5 @@
 CREATE DATABASE pse;
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON `pse_test`.* TO 'student'@'localhost';
 use pse;
+source sql/truncate.sql
 source sql/insert.sql
