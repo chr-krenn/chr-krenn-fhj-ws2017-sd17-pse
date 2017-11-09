031c805d7157585e236a8d2d8b1819400864cd45
moderste15 <stefan.moder@edu.fh-joanneum.at>
2017-11-09 19:52:03 +0100

    Update init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,6 @@
 
 CREATE DATABASE pse;
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON `pse_test`.* TO 'student'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON `pse`.* TO 'student'@'localhost';
 use pse;
 source sql/create.sql
 source sql/insert.sql
