510b791e6c0afc2de6bbb5459aff5fa9a3fc075e
moderste15 <stefan.moder@edu.fh-joanneum.at>
2017-11-13 12:34:39 +0100

    Updated SQL

--- sql/truncate.sql
+++ sql/truncate.sql
@@ -5,7 +5,7 @@
 SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE enumeration;
 TRUNCATE enumeration_item;
-TRUNCATE privateMessage;
+TRUNCATE private_message;
 TRUNCATE contact;
 TRUNCATE post;
 TRUNCATE user_community;
@@ -19,7 +19,7 @@ ALTER TABLE contact AUTO_INCREMENT = 1;
 ALTER TABLE enumeration AUTO_INCREMENT = 1; 
 ALTER TABLE enumeration_item AUTO_INCREMENT = 1; 
 ALTER TABLE post AUTO_INCREMENT = 1; 
-ALTER TABLE privateMessage AUTO_INCREMENT = 1; 
+ALTER TABLE private_message AUTO_INCREMENT = 1; 
 ALTER TABLE user_community AUTO_INCREMENT = 1; 
 ALTER TABLE userprofile AUTO_INCREMENT = 1; 
 ALTER TABLE users AUTO_INCREMENT = 1; 
