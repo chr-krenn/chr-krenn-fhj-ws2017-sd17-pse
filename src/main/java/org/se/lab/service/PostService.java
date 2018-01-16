329e1aee83771468bafa6a46f10b44b15fed2a33
sattlerb10 <bjoern.sattler@edu.fh-joanneum.at>
2018-01-16 22:08:17 +0100

    Removed "Public" identifier in interface

--- src/main/java/org/se/lab/service/PostService.java
+++ src/main/java/org/se/lab/service/PostService.java
@@ -9,8 +9,9 @@ import org.se.lab.data.User;
 
 public interface PostService {
 
-	public Post createPost(User user, String text, Date created) throws DatabaseException;
-	public Post createPost(Post parentpost, Community community, User user, String text, Date created) throws DatabaseException;
-	
-	
+    Post createPost(User user, String text, Date created) throws DatabaseException;
+
+    Post createPost(Post parentpost, Community community, User user, String text, Date created) throws DatabaseException;
+
+
 }
