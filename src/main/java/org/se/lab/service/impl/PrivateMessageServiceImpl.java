b36d944e11d3b99f62edb969920be14c7bac96e4
berdievs14 <b-sandj@hotmail.com>
2018-01-21 01:26:11 +0100

    init privatemessageimpl

---
+++ src/main/java/org/se/lab/service/impl/PrivateMessageServiceImpl.java
@@ -0,0 +1,35 @@
+package org.se.lab.service.impl;
+
+import java.util.List;
+
+import org.se.lab.data.PrivateMessage;
+import org.se.lab.data.User;
+import org.se.lab.service.PrivateMessageService;
+
+public class PrivateMessageServiceImpl implements PrivateMessageService{
+
+	@Override
+	public PrivateMessage findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<PrivateMessage> findMessagesByUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void sendMessage(PrivateMessage privateMessage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteMessage(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
