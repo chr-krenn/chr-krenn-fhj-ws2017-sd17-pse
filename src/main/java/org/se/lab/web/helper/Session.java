8871efd14e9ba2f457f490e985575460367e8376
Bernhard Haring <bernhard.haring@edu.fh-joanneum.at>
2018-02-10 23:13:41 +0100

    added method to session helper to load userId

--- src/main/java/org/se/lab/web/helper/Session.java
+++ src/main/java/org/se/lab/web/helper/Session.java
@@ -1,56 +1,9 @@
 package org.se.lab.web.helper;
 
-import java.io.IOException;
-import java.util.Map;
-
-import javax.ejb.Stateless;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
-import javax.inject.Inject;
-
-import org.apache.log4j.Logger;
 import org.se.lab.db.data.User;
-import org.se.lab.service.UserService;
 
-@Stateless
-public class Session {
-	public final static String USER = "user";
+public interface Session {
 
-	private final Logger LOG = Logger.getLogger(Session.class);
-
-	@Inject
-	private UserService service;
-
-	public User getUser() {
-		final ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
-		final Map<String, Object> session = context.getSessionMap();
-		final Object user = session.get(USER);
-
-		if (user != null) {
-			try {
-				int userId = (int) user;
-				return service.findById(userId);
-			} catch (ClassCastException e) {
-				LOG.error("User in session not well formatted.");
-				redirectLogin(context);
-			}
-		}else{
-			context.invalidateSession();
-			redirectLogin(context);
-		}
-		
-		return null;
-	}
-
-	public void setUser() {
-
-	}
-	
-	private void redirectLogin(ExternalContext context) {
-		try {
-			context.redirect("/pse/login.xhtml");
-		} catch (IOException e) {
-			LOG.error("Can't redirect to /pse/login.xhtml");
-		}
-	}
+    User getUser();
+    int getUserId();
 }
---
+++ src/main/java/org/se/lab/web/helper/SessionHelper.java
@@ -0,0 +1,66 @@
+package org.se.lab.web.helper;
+
+import org.apache.log4j.Logger;
+import org.se.lab.db.data.User;
+import org.se.lab.service.UserService;
+
+import javax.ejb.Stateless;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+
+@Stateless
+public class SessionHelper implements Session {
+    public final static String USER = "user";
+
+    private final Logger LOG = Logger.getLogger(SessionHelper.class);
+
+    @Inject
+    private UserService service;
+
+    public User getUser() {
+        final ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        final Map<String, Object> session = context.getSessionMap();
+        final Object user = session.get(USER);
+
+        if (user != null) {
+            try {
+                int userId = (int) user;
+                return service.findById(userId);
+            } catch (ClassCastException e) {
+                LOG.error("User in session not well formatted.");
+                redirectLogin(context);
+            }
+        } else {
+            context.invalidateSession();
+            redirectLogin(context);
+        }
+
+        return null;
+    }
+
+    public int getUserId() {
+        Object user = FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(USER);
+        if (Objects.nonNull(user)) {
+            try {
+                return (int) user;
+            } catch (ClassCastException e) {
+                LOG.error("User in session not well formatted.");
+            }
+        }
+        return -1;
+
+    }
+
+
+    private void redirectLogin(ExternalContext context) {
+        try {
+            context.redirect("/pse/login.xhtml");
+        } catch (IOException e) {
+            LOG.error("Can't redirect to /pse/login.xhtml");
+        }
+    }
+}
