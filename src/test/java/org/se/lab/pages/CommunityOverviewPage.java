c7c8c947d0e676ed386c337f485a1a373c462a92
j05t <josef.steppan@gmail.com>
2018-02-12 09:46:00 +0100

    fix broken test

--- src/test/java/org/se/lab/pages/CommunityOverviewPage.java
+++ src/test/java/org/se/lab/pages/CommunityOverviewPage.java
@@ -10,11 +10,11 @@ public class CommunityOverviewPage extends PageObject {
 
 	public void createCommunity(String name, String description) {
 		driver.findElement(By.cssSelector("button.btn.btn-success")).click();
-		driver.findElement(By.id("input_j_idt24:j_idt26")).clear();
-		driver.findElement(By.id("input_j_idt24:j_idt26")).sendKeys(name);
-		driver.findElement(By.id("input_j_idt24:j_idt28")).clear();
-		driver.findElement(By.id("input_j_idt24:j_idt28")).sendKeys(description);
-		driver.findElement(By.id("j_idt24:j_idt31")).click();
+		driver.findElement(By.id("input_j_idt24:j_idt27")).clear();
+		driver.findElement(By.id("input_j_idt24:j_idt27")).sendKeys(name);
+		driver.findElement(By.id("input_j_idt24:j_idt29")).clear();
+		driver.findElement(By.id("input_j_idt24:j_idt29")).sendKeys(description);
+		driver.findElement(By.id("j_idt24:sendbutton")).click();
 	}
 
 	public String getCommunityName() {
