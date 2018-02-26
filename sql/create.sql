d5d0cbee4a6f5f4f917e5b3a561d0f26c4e58191
hofchris15 <christian.hofer@edu.fh-joanneum.at>
2018-02-26 08:25:55 +0100

    generate new create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -4,7 +4,7 @@
 -- ------------------------------------------------------
 -- Server version	10.1.26-MariaDB
 
---initialized with mysqldump -d -u root -p pse
+-- created with mysqldump -d -u root -p pse
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,24 +16,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `userprofile`
---
-
-DROP TABLE IF EXISTS `file`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `file` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `users_id` int(11) NULL,
-  `community_id` int(11) NULL,
-  `filename` varchar(255) NOT NULL,
-  `data` mediumblob,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 
 --
 -- Table structure for table `community`
@@ -50,9 +32,10 @@ CREATE TABLE `community` (
   `portaladmin_id` int(11) DEFAULT NULL,
   `enumeration_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_ggi0mfnbrejia9lxku7voffc9` (`name`),
   KEY `FKk7o792d66bqgrp4x310en9a98` (`enumeration_id`),
   CONSTRAINT `FKk7o792d66bqgrp4x310en9a98` FOREIGN KEY (`enumeration_id`) REFERENCES `enumeration` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +52,7 @@ CREATE TABLE `contact` (
   PRIMARY KEY (`id`),
   KEY `FKbxl6anxo14q097g8cd2e51v55` (`user_id`),
   CONSTRAINT `FKbxl6anxo14q097g8cd2e51v55` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -83,7 +66,7 @@ CREATE TABLE `enumeration` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,6 +84,28 @@ CREATE TABLE `enumeration_item` (
   CONSTRAINT `FK2a64bbexb95mu9n8c769rrl0k` FOREIGN KEY (`enumeration_id`) REFERENCES `enumeration` (`id`),
   CONSTRAINT `FK9fjj5j3di6uvh1nylihdj0fh2` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `file`
+--
+
+DROP TABLE IF EXISTS `file`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `file` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `data` mediumblob,
+  `filename` varchar(255) NOT NULL,
+  `community_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_3dj4spomxuc93fn5g89vgx1u` (`filename`),
+  KEY `FKjeurflerqd51hwvqyovv1dnal` (`community_id`),
+  KEY `FKe70ql3orpo0ghvfmqccv27ng` (`user_id`),
+  CONSTRAINT `FKe70ql3orpo0ghvfmqccv27ng` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `FKjeurflerqd51hwvqyovv1dnal` FOREIGN KEY (`community_id`) REFERENCES `community` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -144,6 +149,23 @@ CREATE TABLE `post` (
   CONSTRAINT `FK7u19g6f38sqcrn2o2ot9l3c4s` FOREIGN KEY (`parent_post_id`) REFERENCES `post` (`id`),
   CONSTRAINT `FKg4x80cty3ha7v00q85djomq6d` FOREIGN KEY (`fk_user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `FKjsft64pkupfvvrs6bd4kyokwi` FOREIGN KEY (`fk_community_id`) REFERENCES `community` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=203 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `post_users`
+--
+
+DROP TABLE IF EXISTS `post_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `post_users` (
+  `Post_id` int(11) NOT NULL,
+  `likedByUsers_id` int(11) NOT NULL,
+  KEY `FKmrbiwgfd5dfevlcichn72rsy` (`likedByUsers_id`),
+  KEY `FKjpqym9kc0w6m82ouanq0npqbh` (`Post_id`),
+  CONSTRAINT `FKjpqym9kc0w6m82ouanq0npqbh` FOREIGN KEY (`Post_id`) REFERENCES `post` (`id`),
+  CONSTRAINT `FKmrbiwgfd5dfevlcichn72rsy` FOREIGN KEY (`likedByUsers_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -164,7 +186,7 @@ CREATE TABLE `private_message` (
   KEY `FK9jb3g7xe0cy4sjav6fk34dkhg` (`fk_user_id_sender`),
   CONSTRAINT `FK9jb3g7xe0cy4sjav6fk34dkhg` FOREIGN KEY (`fk_user_id_sender`) REFERENCES `users` (`id`),
   CONSTRAINT `FKmjbhmvq38xehsknjxd2jl7r0u` FOREIGN KEY (`fk_user_id_receiver`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -207,7 +229,7 @@ CREATE TABLE `userprofile` (
   `room` varchar(255) DEFAULT NULL,
   `team` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -226,7 +248,7 @@ CREATE TABLE `users` (
   UNIQUE KEY `UK_r43af9ap4edm43mmtq01oddj6` (`username`),
   KEY `FKcmombhhasank864ums4swd519` (`fk_userprofile`),
   CONSTRAINT `FKcmombhhasank864ums4swd519` FOREIGN KEY (`fk_userprofile`) REFERENCES `userprofile` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -238,4 +260,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-01-18 15:11:26
+-- Dump completed on 2018-02-26  8:20:57
