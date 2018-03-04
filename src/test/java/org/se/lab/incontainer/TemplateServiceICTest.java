88eb4a665cf7d64a03ae769a759539993e2d9bab
hofchris15 <christian.hofer@edu.fh-joanneum.at>
2018-03-04 22:17:32 +0100

    remove further system.outs

--- src/test/java/org/se/lab/incontainer/TemplateServiceICTest.java
+++ src/test/java/org/se/lab/incontainer/TemplateServiceICTest.java
@@ -19,33 +19,38 @@ import javax.transaction.UserTransaction;
 
 @RunWith(Arquillian.class)
 public abstract class TemplateServiceICTest {
-	
-	@Inject protected UserService userService;
-	@Inject protected ActivityStreamService activityStreamService;
-	@Inject protected CommunityService communityService;
-	@Inject protected EnumerationService enumerationService;
-	@Inject protected PostService postService;
-	@Inject protected PrivateMessageService pmService;
-	@Inject protected EnumerationDAO enumDao;
-	@Inject protected UserContactDAO contactDao;
-	@Inject protected UserProfileDAO profileDao;
-	
+
+	@Inject
+	protected UserService userService;
+	@Inject
+	protected ActivityStreamService activityStreamService;
+	@Inject
+	protected CommunityService communityService;
+	@Inject
+	protected EnumerationService enumerationService;
+	@Inject
+	protected PostService postService;
+	@Inject
+	protected PrivateMessageService pmService;
+	@Inject
+	protected EnumerationDAO enumDao;
+	@Inject
+	protected UserContactDAO contactDao;
+	@Inject
+	protected UserProfileDAO profileDao;
+
 	protected boolean isset = false;
-	
-	@Inject protected UserTransaction tx;
-	
+
+	@Inject
+	protected UserTransaction tx;
+
 	@Deployment
 	public static JavaArchive createDeployment() {
 		JavaArchive jar = ShrinkWrap.create(JavaArchive.class, "pse.jar")
-				
-				.addPackages(true, "org.se.lab.db")
-				.addPackages(true, "org.se.lab.service")
-				.addPackages(true, "org.se.lab.utils")
-				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
-				.addAsManifestResource("META-INF/arquillian_db.xml", "persistence.xml")
-				//.addAsManifestResource("META-INF/arquillian.xml", "arquillian.xml")
-				;		
-		System.out.println(jar.toString(true));
+
+				.addPackages(true, "org.se.lab.db").addPackages(true, "org.se.lab.service")
+				.addPackages(true, "org.se.lab.utils").addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+				.addAsManifestResource("META-INF/arquillian_db.xml", "persistence.xml");
 		return jar;
 	}
 
@@ -53,18 +58,18 @@ public abstract class TemplateServiceICTest {
 	public void setup() throws Exception {
 		tx.begin();
 		createEnums();
-		
+
 		tx.commit();
-		
+
 		tx.begin();
-		
+
 	}
-	
+
 	public void createEnums() {
-		
-		if(isset) {
+
+		if (isset) {
 			return;
-		}else {
+		} else {
 			enumDao.createEnumeration(1);
 			enumDao.createEnumeration(2);
 			enumDao.createEnumeration(3);
@@ -75,16 +80,13 @@ public abstract class TemplateServiceICTest {
 			enumDao.createEnumeration(8);
 			isset = true;
 		}
-		
 
 	}
-	
-	
+
 	@After
 	public void tearDown() throws Exception {
-		
+
 		tx.rollback();
 	}
-	
 
 }
